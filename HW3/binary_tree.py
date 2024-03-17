23c29377ed0da0107f31dbd7837b0103ca681f26
Kiran Kodali <kirankodali10@icloud.com>
2024-03-16 20:04:11 -0700

    updated topology

--- HW3/binary_tree.py
+++ HW3/binary_tree.py
@@ -1,14 +1,14 @@
 from mininet.topo import Topo
 
-
-class BinaryTreeTopo(Topo):
+class BinaryTreeTopo( Topo ):
     "Binary Tree Topology Class."
 
-    def __init__(self):
+    def __init__( self ):
         "Create the binary tree topology."
 
         # Initialize topology
-        Topo.__init__(self)
+        Topo.__init__( self )
+
         # Add hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
@@ -18,7 +18,7 @@ class BinaryTreeTopo(Topo):
         h6 = self.addHost('h6')
         h7 = self.addHost('h7')
         h8 = self.addHost('h8')
-
+        
         # Add switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
@@ -29,20 +29,26 @@ class BinaryTreeTopo(Topo):
         s7 = self.addSwitch('s7')
 
         # Add links
-        self.addLink(s1, s2)
-        self.addLink(s1, s5)
-        self.addLink(s2, s3)
-        self.addLink(s2, s4)
-        self.addLink(s3, h1)
-        self.addLink(s3, h2)
-        self.addLink(s4, h3)
-        self.addLink(s4, h4)
-        self.addLink(s5, s6)
-        self.addLink(s5, h7)
-        self.addLink(s6, h5)
-        self.addLink(s6, h6)
-        self.addLink(s7, h7)
-        self.addLink(s7, h8)
+        self.addLink(h1,s3)
+        self.addLink(h2,s3)
+        
+        self.addLink(h3,s4)
+        self.addLink(h4,s4)
+        
+        self.addLink(h5,s6)
+        self.addLink(h6,s6)
+        
+        self.addLink(h7,s7)
+        self.addLink(h8,s7)
+        
+        self.addLink(s3,s2)
+        self.addLink(s4,s2)
+        
+        self.addLink(s6,s5)
+        self.addLink(s7,s5)
+        
+        self.addLink(s2,s1)
+        self.addLink(s5,s1)
 
 
-topos = {'binary_tree': (lambda: BinaryTreeTopo())}
+topos = { 'binary_tree': ( lambda: BinaryTreeTopo() ) }
